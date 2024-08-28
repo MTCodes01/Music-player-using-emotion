66dd79b2e2900687fdae1e4c2ad4830b64d45d98
Sreedev S S <108288413+MTCodes01@users.noreply.github.com>
2024-08-28 17:52:20 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Function to redirect to login.html after animation completes
-function redirectToLogin() {
-  window.location.href = "./Webpage/index.html"; // Redirecting to login.html
-}
-// Set a timeout to redirect after 4 seconds (2 cycles)
-setTimeout(redirectToLogin, 4000); // Setting a timeout for redirection
